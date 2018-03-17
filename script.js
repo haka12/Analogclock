6ef813cf7d54b5ffa154c5113e2c985fea2858c4
haka12 <bakalantern@gmail.com>
2018-03-17 12:04:30 +0545

    Adding loop

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 var secondhand = document.querySelector("#secondhand");
 var minutehand = document.querySelector("#minutehand");
 var hourhand = document.querySelector("#hourhand");
-
-   one= document.querySelector("#one");
-   one.style.transformOrigin="100px 100px";
-   one.style.transform="rotate("+30+"deg)";
-
+var num=1;
+var rotdeg=30;
+while(num<13){
+    rotateno(num);
+    num++;
+}
+   
+function rotateno(num){
+    document.querySelector("#one").innerHTML=num;
+    one= document.querySelector("#one");
+    one.style.transformOrigin="100px 100px";
+    one.style.transform="rotate("+rotdeg+"deg)";
+    console.log(num+":"+ rotdeg);
+    rotdeg+=30;
+}
 function update(){
     var date = new Date();
-    console.log(date);
+  //  console.log(date);
     var hrs = date.getHours();
     var mins = date.getMinutes();
     var sec = date.getSeconds();
     secdeg= sec*(360/60);
     mindeg= mins*(360/60)+sec*(1/10);
     hrsdeg=hrs*(360/12)+mins*(1/2);
-    console.log(hrs + ":" + mins + ":" + sec)
+   // console.log(hrs + ":" + mins + ":" + sec)
     secondhand.style.transform = "rotate("+secdeg+"deg)";
     minutehand.style.transform = "rotate("+mindeg+"deg)";
     hourhand.style.transform = "rotate("+hrsdeg+"deg)";
 }
 setInterval(update,1000)
+
